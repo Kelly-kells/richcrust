31b44af211a1cd849574281658b83bb66baa341f
unknown <kellskelechi@gmail.com>
2024-01-01 20:14:12 +0100

    notification

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Notification = () => {
-  return (
-    <div>Notification</div>
-  )
+    return (
+        <div className=' h-12 bg-gray-950 text-slate-200 px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>Free delivery for orders above $20. Order your pizza now!</div>
+    )
 }
 
 export default Notification

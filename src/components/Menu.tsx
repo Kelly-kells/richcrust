a86c5e726b1c86d7470addd636cb798aa358d715
unknown <kellskelechi@gmail.com>
2024-01-01 22:36:57 +0100

    navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
 
-const Menu = () => {
-  return (
-    <div>Menu</div>
-  )
+import Link from "next/link";
+import { useState } from "react";
+
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
+
+const navMenu = () => {
+
+    const IconStyle = {
+        color: '#e4b61a',
+        fontSize: '24px',
+    };
+    const [open, setOpen] = useState(false)
+    const user= false
+
+    const links = [
+        { id: 1, title: "Home", url: "/" },
+        { id: 2, title: "Menu", url: "/menu" },
+        { id: 3, title: "Working Hours", url: "/working hours" },
+        { id: 4, title: "contact", url: "/ contact" }
+    ]
+    return (
+        <div>
+            {!open ? (
+                <GiHamburgerMenu style={IconStyle} onClick={() => setOpen(true)} />
+            ) : (
+                <MdClose style={IconStyle} onClick={() => setOpen(false)} />
+            )
+            }
+            <div className="bg-gray-950 text-white 
+            absolute left-0 top-24 h-1/2 w-40 rounded-md flex flex-col items-center  justify-center gap-4 z-10 ">
+                {links.map(item => (
+                    <Link href={item.url} key={item.id}>
+                        {item.title}
+                    </Link>
+                ))}
+                {!user ? (
+                <Link href="/login">Login</Link>
+                ):
+               ( <Link href="/order">Order</Link>)}
+               <Link href="/cart"></Link>
+            </div>
+        </div>
+    )
 }
-
-export default Menu
+export default navMenu
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Menu from "./Menu"
+import Link from 'next/link'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className=' h-12 text-black p-4 flex items-center justify-between border-b-2 border-b-primary shadow-md'>
+      {/*logo*/}
+      <div >
+      <Link href='/'>
+        RichCrust
+      </Link>
+      </div>
+       {/*mobile-menu*/}
+       <div>
+        <Menu/>
+       </div>
+    </div>
   )
 }
 

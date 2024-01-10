cf23f7ddb108404486a3b908d92a07358b845b0b
unknown <kellskelechi@gmail.com>
2024-01-10 16:14:50 +0100

    products

--- src/components/FeaturedItems.tsx
+++ src/components/FeaturedItems.tsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const FeaturedItems = () => {
   return (
-    <div>FeaturedItems</div>
+    <div className=' w-screen overflow-x-scroll text-black'>
+      {/*WRAPPER */}
+      <div className=" w-max flex">
+        {/*SINGLE Container */}
+
+        <div className="w-screen h-[50vh] flex  flex-col items-center justify-around">
+          {/*Image container */}
+          <div className="relative ">
+            <img src="/pizza-menu/pepperoni.png" alt="" width={200} />
+          </div>
+
+          <div className="">
+            <h1>Title</h1>
+            <p>discription</p>
+            <span>price</span>
+            <button>Add to Cart</button>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 

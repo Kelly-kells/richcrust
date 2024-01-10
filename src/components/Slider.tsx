4be2b85e3e65e765eaac0d606ac5313b91f0bc3b
unknown <kellskelechi@gmail.com>
2024-01-10 13:46:43 +0100

    hero-section completed

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,39 +1,48 @@
 "use client";
-import React, { useState } from 'react';
-
-
+import React, { useEffect, useState } from 'react';
 import 'animate.css';
 
-const data =[
-  {id: 1,
+const data = [
+  {
+    id: 1,
     title: "Always Fresh and Sumptuous",
-    Image:"./pizza.jpg"
+    Image: "./herobg1.jpg"
   },
 
-  {id: 2,
+  {
+    id: 2,
     title: "The best pizza to share with your family",
-    Image:"./pizza2.jpg"
+    Image: "./pizza2.jpg"
   },
 
-  {id: 3,
+  {
+    id: 3,
     title: "We make deliveries everywhere within Abuja",
-    Image:"./delivery.jpg"
+    Image: "./delivery.jpg"
   },
 ]
 
 const Slider = () => {
 
-  const[currentSlider, setCurrentSlider]=useState(0)
+  const [currentSlider, setCurrentSlider] = useState(0)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlider((prev) => (prev + 1) % data.length); // Use modulo to loop back to the first item
+    }, 3000);;
+
+
+    return () => clearInterval(interval);
+  }, []);
   return (
- 
-    <div className="bg-cover bg-center h-screen flex items-center justify-center"
-    style={{ backgroundImage: `url(${data[currentSlider].Image})` }}>
- <div className='bg-primary  m-4 p-8 opacity-85'>
- <h1 className="text-white text-4xl md:text-5xl w-full text-center font-extrabold animate__animated animate__bounceInDown ">{data[currentSlider].title}
-</h1>
-<button className='bg-black text-primary text-xl md:text-2xl ml-8 font-semibold rounded-md p-2 mt-6'>Order now!</button>
- </div>
-</div>
+
+    <div className="bg-cover bg-center h-screen flex items-center justify-center transition-opacity ease-in-out"
+      style={{ backgroundImage: `url(${data[currentSlider].Image})` }}>
+      <div className='bg-primary  m-4 p-8 opacity-85'>
+        <h1 className="text-white text-4xl md:text-5xl w-full text-center font-extrabold animate__animated animate__bounceInDown ">{data[currentSlider].title}
+        </h1>
+        <button className='bg-black text-primary text-xl md:text-2xl ml-8 font-semibold rounded-md p-2 mt-6'>Order now!</button>
+      </div>
+    </div>
   )
 }
 

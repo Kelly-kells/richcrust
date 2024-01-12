814bc55ab3a64068c15bb3ea109e435577d6a9c7
unknown <kellskelechi@gmail.com>
2024-01-13 00:24:59 +0100

    menu responsive

--- src/components/FeaturedItems.tsx
+++ src/components/FeaturedItems.tsx
@@ -9,35 +9,32 @@ const FeaturedItems = () => {
       {/*WRAPPER */}
       <div className=" w-max flex">
         {/*SINGLE Container */}
-        {featuredProducts.map((item)=>(
+        {featuredProducts.map((item) => (
 
-<div key={item.id} className="w-screen h-[60vh] flex  flex-col items-center 
+          <div key={item.id} className="w-screen h-[60vh] flex  flex-col items-center 
 justify-around p-4 hover:transition-all duration-300 md:w-[50vw] xl:w-[33vw] lg:h-[100vh]">
-{/*Image container */}
+            {/*Image container */}
 
-{item.img &&(
-  <div className="relative mt-2 hover:rotate-[60deg] transition-all duration-500">
-  <img src={item.img} alt="" width={250} className='object-contain' />
-  
-</div>
-)}
+            {item.img && (
+              <div className="relative mt-2 hover:rotate-[60deg] transition-all duration-500">
+                <img src={item.img} alt="" width={250} className='object-contain' />
+
+              </div>
+            )}
+
+            <div className=" flex flex-col justify-center items-center text-center gap-2">
+              <h1 className=' text-xl font-bold uppercase'>{item.title}</h1>
+              <p className=''>{item.desc}</p>
+              <span className='text-xl font-bold'>${item.price}</span>
+              <button className='bg-black text-primary p-2 mt-4 font-medium rounded-md'>Add to Cart</button>
+            </div>
+          </div>
+
+        )
 
 
-<div className=" flex flex-col justify-center items-center text-center gap-2">
-<h1 className=' text-xl font-bold uppercase'>{item.title}</h1>
-  <p className=''>{item.desc}</p>
-  <span className='text-xl font-bold'>${item.price}</span>
-  <button className='bg-black text-primary p-2 mt-4 font-medium rounded-md'>Add to Cart</button>
-</div>
-
-
-</div>
-
-        ) 
-        
-        
         )}
-       
+
       </div>
 
     </div>

54cf11e684a4dac43cb792a411646a8eaa74820a
unknown <kellskelechi@gmail.com>
2024-01-18 18:12:57 +0100

    countdown

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Offer = () => {
   return (
     <div className='bg-black h-screen flex flex-col md:flex-row'>
-      <div className='flex-1 flex-col justify-center items-center text-center gap-8 p-6 text-white '>
+      <div className='flex-1 flex flex-col justify-center items-center text-center gap-8 p-6 text-white '>
         <h1 className='text-5xl font-bold'> Our Delicious and creamy Burger offer </h1>
-        <p> Get our large burger when you buy a $50 worth of pizza</p>
-        <button className='bg-primary text-black p-2 mt-4 font-medium rounded-md'>Order Now</button>
+        <p className=" xl:text-xl"> Get our large burger when you buy a $50 worth of pizza</p>
+        <button className=' bg-red-700 text-white py-3 px-6 mt-4 font-bold rounded-md '>Order Now</button>
       </div>
-      <div className='flex-1 w-full relative'>
+      <div className='flex-1 w-full relative animate-bounce'>
         <img src="/offer/burger.png" alt="" className='object-cover' />
       </div>
     </div>

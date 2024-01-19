6dd162543e21baa727f893a3d510f857a715f0b7
unknown <kellskelechi@gmail.com>
2024-01-19 19:58:30 +0100

    updated

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,9 +1,15 @@
+import { menu } from '@/data'
+import Link from 'next/link'
 import React from 'react'
 
 const menuPage = () => {
   return (
-    <div className='p-4 lg:p-20 xl:p-10 flex flex-col md:flex-row'>
-      MENU
+    <div className='p-4 lg:p-20 xl:p-10 flex flex-col md:flex-row items-center'>
+     {menu.map(category=>(
+      <Link href={category.slug} key={category.id}>
+
+      </Link>
+     ))}
       </div>
   )
 }
--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,17 +1,34 @@
-"use client";
-import React from 'react';
-import Countdown from 'react-countdown';
+"use client"
+import React, { useState, useEffect } from "react";
 
 const CountDown = () => {
-  const endDate = new Date('2024-03-15');
+  
+  let difference = +new Date(`03/03/2024`) - +new Date();
+  const [delay, setDelay] = useState(difference);
 
+  const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const h = Math.floor((difference / (1000 * 60 * 60)) % 24);
+  const m = Math.floor((difference / 1000 / 60) % 60);
+  const s = Math.floor((difference / 1000) % 60);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDelay(delay - 1);
+    }, 1000);
+
+    if (delay === 0) {
+      clearInterval(timer);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  });
   return (
-    <div>
-      <Countdown className='text-5xl text-primary font-bold' date={endDate} />
-    </div>
+    <span className="font-bold text-5xl text-yellow-300">
+      {d}:{h}:{m}:{s}
+    </span>
   );
 };
 
 export default CountDown;
-;
-
